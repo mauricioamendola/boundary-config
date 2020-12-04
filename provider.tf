0bd23bd0d602105e53a003b1ea8cd38045d6e343
Mauricio Amendola <mauricio@linux.com.uy>
2020-12-04 07:52:39 -0300

    added terraform provider

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "boundary" {
+  addr                            = "http://127.0.0.1:9200"
+  auth_method_id                  = "ampw_daC3yFLWW7" # changeme
+  password_auth_method_login_name = "admin"          # changeme
+  password_auth_method_password   = "LBwTg8UPK18IGndipQBG"        # changeme
+}
